f290a4e38bdcd387baa0d520f300e0e5dfc1bfec
msavaa <avasophiagnzls@gmail.com>
2025-11-20 00:37:04 +0800

    Health
    Updated class name and method for clarity and correctness.

--- src/HomeBudget/Health.java
+++ src/HomeBudget/Health.java
@@ -1,15 +1,15 @@
-package HomeBudget;
+package HomeBudget; //HomeBudget is where related classes are stored.
 
-public class Health extends ExpensesCategory {
+public class Health extends ExpensesCategories { // ExpensesCategories is the parent class. While the Health is the child class. 
     public Health(double limit) {
         super("Health", limit);
     }
 
-    // Polymorphism 
+    @Override
     public String getHealthReminder() {
-        if (isitWithinLimit()) 
+        if (isWithinLimit())
             return "Nice! Health is wealth. Investing in your health is the best insurance!";
-        else 
+        else
             return "Whoa there, buddy! At this rate, your wallet might need medical attention too!";
     }
 }
