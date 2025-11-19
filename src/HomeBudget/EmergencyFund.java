1f946be117a4fa5cdc28b428608b2cc2f23bd4e2
msavaa <avasophiagnzls@gmail.com>
2025-11-20 00:35:20 +0800

    EmergencyFund
    Updated comments for clarity regarding inheritance and polymorphism.

--- src/HomeBudget/EmergencyFund.java
+++ src/HomeBudget/EmergencyFund.java
@@ -1,15 +1,20 @@
-package HomeBudget;
+package HomeBudget; //HomeBudget is where related classes are stored.
 
-public class EmergencyFund extends ExpensesCategory {
+// This is an INHERITANCE.
+
+public class EmergencyFund extends ExpensesCategories { // ExpensesCategories is the parent class. While the EmergencyFund is the child class.
     public EmergencyFund(double limit) {
         super("Emergency Fund", limit);
     }
 
-    // Polymorphism 
+    @Override
     public String getEmergencyFundReminder() {
-        if (isitWithinLimit()) 
+    // This is a POLYMORPHISM - as this is in the parent class method while the other one is at the child class (EmergencyFund.ExpensesCategories) which overrides it. 
+    // Has same method name (getEmergencyFundReminder, etc.,) but has different behavior - the parent returns an empty string while the child class returns a message.
+
+        if (isWithinLimit())
             return "Smart move! Future you just smiled.";
-        else 
+        else
             return "If this is an emergency, your wallet definitely knows the feeling now!";
     }
 }
